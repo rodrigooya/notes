36072b9d605ee9f8de4bbb9857ff1f4dd1522f65
Rodrigo Oyarzun <89427203+rodrigooya@users.noreply.github.com>
2023-11-02 18:09:26 -0300

    Agregar Frontend notes

---
+++ backend/build/precache-manifest.25abca8ebd76e078da9f36fc1fd3f04a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d92a711f34fe5e1e74e",
+    "url": "/static/css/main.70998b3c.chunk.css"
+  },
+  {
+    "revision": "2d92a711f34fe5e1e74e",
+    "url": "/static/js/main.2d92a711.chunk.js"
+  },
+  {
+    "revision": "544df384feb66c43482b",
+    "url": "/static/js/1.544df384.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afb8e7960d1a36ea2cef1419c500d5ba",
+    "url": "/index.html"
+  }
+];
--- backend/index.js
+++ backend/index.js
@@ -13,36 +13,37 @@ const requestLogger = (request, response, next) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
+
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
+app.use(requestLogger)
 
 let notes = [  
     {    
         id: 1,    
-        content: "HTML is easy",    
-        date: "2019-05-30T17:30:31.098Z",    
+        content: "HTML is easy",       
         important: true  
     },  
     {    
         id: 2,    
-        content: "Browser can execute only Javascript",    
-        date: "2019-05-30T18:39:34.091Z",    
+        content: "Browser can execute only Javascript",       
         important: false  
     },  
     {    
         id: 3,    
-        content: "GET and POST are the most important methods of HTTP protocol",    
-        date: "2019-05-30T19:20:14.298Z",    
+        content: "GET and POST are the most important methods of HTTP protocol",      
         important: true  
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
 })
   
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
 })
 
 const generateId = () => {
